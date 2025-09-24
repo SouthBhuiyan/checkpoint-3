240dabccbe692a0cf3674c3cf2ccece6a31c6e35
Md South Bhuiyan <southbhuiyan98@gmail.com>
2025-09-24 19:58:02 -0400

    Ignore coverage in Prettier and fix test formatting

--- __tests__/math.test.js
+++ __tests__/math.test.js
@@ -1,6 +1,6 @@
 // __tests__/math.test.js
-const { add } = require('../math');
+const { add } = require("../math");
 
-test('adds 2 + 3 to equal 5', () => {
+test("adds 2 + 3 to equal 5", () => {
   expect(add(2, 3)).toBe(5);
 });
